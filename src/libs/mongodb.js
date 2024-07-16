824225ff13e88ae2c4099608f00d9e9c3330eea7
Suchana Nag <99524477+SuchanaNag002@users.noreply.github.com>
2024-07-16 13:17:20 +0530

    Added blog model and modified mongodb connection strings

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -1,25 +1,39 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = async () => {
-    try {
-      await mongoose
-        .connect(
-          process.env.NEXT_PUBLIC_MONGODB_URL,
-          {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          }
-        )
-        .then(() => {
-          console.log("DB connected");
-        })
-        .catch((error) => {
-          console.error("Error connecting to MongoDB:", error);
-        });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  export default connectMongoDB;
-  
+const connectImagesDB = async () => {
+  try {
+    await mongoose
+      .connect(process.env.NEXT_PUBLIC_MONGODB_URL_IMAGES, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Images DB connected");
+      })
+      .catch((error) => {
+        console.error("Error connecting to Images MongoDB:", error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const connectBlogsDB = async () => {
+  try {
+    await mongoose
+      .connect(process.env.NEXT_PUBLIC_MONGODB_URL_TEXT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Blogs DB connected");
+      })
+      .catch((error) => {
+        console.error("Error connecting to Blogs MongoDB:", error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { connectImagesDB, connectBlogsDB };
---
+++ src/models/BlogModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+let Blog;
+try {
+  // Attempt to fetch the existing model if it exists
+  Blog = mongoose.model("Blog");
+} catch (error) {
+  // If the model doesn't exist, define it
+  const BlogSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+  Blog = mongoose.model("Blog", BlogSchema);
+}
+
+export default Blog;
